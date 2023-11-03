2573c033983f3eec940b482e9eb2b3b8c857d958
Bas Kaal <3askaal@gmail.com>
2023-11-04 00:01:31 +0100

    refactor controls component

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,4 +1,4 @@
-import { Box, Spacer, Button, theme } from "3oilerplate";
+import { Spacer, s } from "3oilerplate";
 import { ChevronLeft, ChevronRight, ChevronUp, ChevronDown, ChevronsDown, RotateCw, Move } from "react-feather";
 
 interface ControlsProps {
@@ -7,42 +7,123 @@ interface ControlsProps {
   rotation: number;
 }
 
+const SControlPad = s.div(({ rotation }: { rotation: number }) => ({
+  position: 'relative',
+  width: '8rem',
+  height: '8rem',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  transform: `rotate(${rotation}deg)`,
+}));
+
+const SControlPadButton = s.button(({ direction }: { direction: string }) => ({
+  position: 'absolute',
+  px: 'xs',
+  py: 'xxs',
+  margin: 'auto',
+  width: '3rem',
+  height: '3rem',
+  borderRadius: '.5rem',
+  border: '0 !important',
+  background: 'transparent !important',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  svg: {
+    stroke: 'secondaryDark',
+    width: '32px',
+    height: '32px'
+  },
+
+  ...(direction === 'up' && {
+    top: 0,
+    left: 0,
+    right: 0,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+  }),
+
+  ...(direction === 'down' && {
+    left: 0,
+    right: 0,
+    bottom: 0,
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0,
+  }),
+
+  ...(direction === 'left' && {
+    top: 0,
+    left: 0,
+    bottom: 0,
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+  }),
+
+  ...(direction === 'right' && {
+    top: 0,
+    right: 0,
+    bottom: 0,
+    borderTopLeftRadius: 0,
+    borderBottomLeftRadius: 0,
+  })
+}));
+
+const SControlPadIcon = s.div(() => ({
+  pointerEvents: 'none',
+
+  svg: {
+    stroke: 'greys.90'
+  }
+}));
+
+const ControlPad = ({ children, onUp, onDown, onLeft, onRight, rotation }: any) => {
+  return (
+    <SControlPad rotation={rotation}>
+      <SControlPadButton direction="up" onClick={onUp}>
+        <ChevronUp/>
+      </SControlPadButton>
+      <SControlPadButton direction="left" onClick={onLeft}>
+        <ChevronLeft/>
+      </SControlPadButton>
+      <SControlPadButton direction="right" onClick={onRight}>
+        <ChevronRight/>
+      </SControlPadButton>
+      <SControlPadButton direction="down" onClick={onDown}>
+        <ChevronDown/>
+      </SControlPadButton>
+      <SControlPadIcon>
+        { children }
+      </SControlPadIcon>
+    </SControlPad>
+  )
+}
+
 export const Controls = ({ onRotate, onReposition, rotation }: ControlsProps) => {
   return (
-    <Spacer size="xs" s={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-      <Box posr s={{ mb: 0, transform: `rotate(${rotation}deg)` }}>
-        <Box df w100p jcc s={{ flexGrow: 1 }}>
-          <Button ver onClick={() => onRotate('x', 'ccw')}><ChevronUp /></Button>
-        </Box>
-        <Box df w100p jcc s={{ flexGrow: 1 }}>
-          <Button hor onClick={() => onRotate('z', 'ccw')}><ChevronLeft /></Button>
-          <Button hor onClick={() => onRotate('z', 'cw')}><ChevronRight /></Button>
-        </Box>
-        <Box df w100p jcc s={{ flexGrow: 1 }}>
-          <Button ver onClick={() => onRotate('x', 'cw')}><ChevronDown /></Button>
-        </Box>
-        <Box df posa aic jcc r={0} l={0} b={0} t={0} s={{ svg: { stroke: theme.colors.greys['90'] }, pointerEvents: 'none' }}>
-          <RotateCw size={20}/>
-        </Box>
-      </Box>
-      <Box s={{ mb: 0 }}>
-        <Button mid onClick={() => onReposition('y', -1)}><ChevronsDown /></Button>
-      </Box>
-      <Box posr s={{ mb: 0, transform: `rotate(${rotation}deg)` }}>
-        <Box df w100p jcc s={{ flexGrow: 1 }}>
-          <Button ver onClick={() => onReposition('z', -1)}><ChevronUp /></Button>
-        </Box>
-        <Box df w100p jcc s={{ flexGrow: 1 }}>
-          <Button hor onClick={() => onReposition('x', -1)}><ChevronLeft /></Button>
-          <Button hor onClick={() => onReposition('x', 1)}><ChevronRight /></Button>
-        </Box>
-        <Box df w100p jcc s={{ flexGrow: 1 }}>
-          <Button ver onClick={() => onReposition('z', 1)}><ChevronDown /></Button>
-        </Box>
-        <Box df posa aic jcc r={0} l={0} b={0} t={0} s={{ svg: { stroke: theme.colors.greys['90'] }, pointerEvents: 'none' }}>
-          <Move size={20}/>
-        </Box>
-      </Box>
+    <Spacer size="xl" s={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+      <ControlPad
+        onUp={() => onRotate('x', 'ccw')}
+        onLeft={() => onRotate('z', 'ccw')}
+        onRight={() => onRotate('z', 'cw')}
+        onDown={() => onRotate('x', 'cw')}
+        rotation={rotation}
+      >
+        <RotateCw size={20}/>
+      </ControlPad>
+      <SControlPadButton onClick={() => onReposition('y', -1)}>
+        <ChevronsDown />
+      </SControlPadButton>
+      <ControlPad
+        onUp={() => onReposition('z', -1)}
+        onLeft={() => onReposition('x', -1)}
+        onRight={() => onReposition('x', 1)}
+        onDown={() => onReposition('z', 1)}
+        rotation={rotation}
+      >
+        <Move size={20}/>
+      </ControlPad>
     </Spacer>
   )
 }
