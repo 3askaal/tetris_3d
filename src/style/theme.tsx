526fdeda4cc008f06ebc1213b686adf0d24c0ac8
Bas Kaal <3askaal@gmail.com>
2023-11-05 17:29:50 +0100

    simplify usage of component library

--- src/style/theme.tsx
+++ src/style/theme.tsx
@@ -3,7 +3,7 @@ import { darken, brighten } from '3oilerplate'
 const black = brighten('#000', .25);
 const white = darken('#fff', .25);
 
-export const LOCAL_THEME = {
+export const THEME = {
   rootFontSizes: ['10px', '16px'],
   colors: {
     black,
